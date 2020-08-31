e7bd34b032e982df8d93391bae555aa2d346194f
Keshvi Mahalingam <keshmahal@gmail.com>
2020-08-31 13:45:07 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/cs2201Projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs2201Projects.dir/main.cpp.obj"
+  "cs2201Projects.exe"
+  "cs2201Projects.exe.manifest"
+  "cs2201Projects.pdb"
+  "libcs2201Projects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs2201Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
